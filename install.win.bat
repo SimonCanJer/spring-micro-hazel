b7a670b14caa5dc9016fcd1aa22d188d197d60b6
Simon <SimonCanJer@gmail.com>
2020-02-23 10:42:26 +0200

    packaging and execution related changes done

---
+++ install.win.bat
@@ -0,0 +1,15 @@
+set mypath=%cd%
+echo %mypath%
+mkdir %1
+cd %1
+git clone https://SimonCanJer:PolarWolf1957@github.com/SimonCanJer/microhazle.git
+cd microhazle
+call mvn install
+cd %mypath%
+call mvn install
+cd micro-hazel-spring-examples\target
+start  java -jar  micro-hazel-spring-examples-1.0.4-exec.jar  backend
+timeout 45
+start  java -jar  micro-hazel-spring-examples-1.0.4-exec.jar  facade
+cd %mypath%
+
