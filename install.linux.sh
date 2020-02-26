c74dff948134aeeb804da2b14b5dc7a3557c8d85
Simon <SimonCanJer@gmail.com>
2020-02-26 10:20:01 +0200

    packaging and execution related changes done

--- install.linux.sh
+++ install.linux.sh
@@ -1,15 +1,14 @@
-mypath=$pwd
-echo $mypath
-sudo mkdir $1
-cd $1
-git clone https://SimonCanJer:PolarWolf1957@github.com/SimonCanJer/microhazle.git
-cd microhazle
-call mvn install
-cd $mypath
-exec mvn install
-cd micro-hazel-spring-examples\target
-exec  java -jar micro-hazel-spring-examples-1.0.4-exec.jar  backend &
+mypath="$(pwd)"
+sudo  mkdir "$1"
+cd   "$1" || return
+git   clone https://SimonCanJer:PolarWolf1957@github.com/SimonCanJer/microhazle.git
+cd    microhazle
+call   mvn install
+cd    "$mypath" || return
+exec   mvn install
+cd     micro-hazel-spring-examples\target || return
+exec   java -jar micro-hazel-spring-examples-1.0.4-exec.jar  backend &
 sleep 40
-exec java -jar micro-hazel-spring-examples-1.0.4-exec.jar  facade  &
-cd $mypath
+exec   java -jar micro-hazel-spring-examples-1.0.4-exec.jar  facade  &
+cd     "$mypath"
 
