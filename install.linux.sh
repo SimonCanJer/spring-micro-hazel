0b0b2a2cd0b2c82bbfcb24f7f5015b5ecb5381cd
Simon <SimonCanJer@gmail.com>
2020-02-26 18:49:12 +0200

    formatting

--- install.linux.sh
+++ install.linux.sh
@@ -3,10 +3,10 @@ sudo  mkdir -p "$1"
 cd   "$1" || return
 git   clone https://SimonCanJer:PolarWolf1957@github.com/SimonCanJer/microhazle.git
 cd    microhazle || exit
-mvn install
+mvn    install
 cd    "$mypath" || return
-mvn install
-cd     micro-hazel-spring-examples\target || return
+mvn     install
+cd     micro-hazel-spring-examples/target || return
         java -jar micro-hazel-spring-examples-1.0.4-exec.jar  backend &
 sleep 40
 exec   java -jar micro-hazel-spring-examples-1.0.4-exec.jar  facade  &
